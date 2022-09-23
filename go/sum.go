deb9cc609c097c04748eef98f53cf1de290fb9ed
henriquelucasdf <henriquelucasdf@gmail.com>
2022-09-22 22:45:26 -0300

    feat: added subtract function in new branch

--- go/sum.go
+++ go/sum.go
@@ -4,8 +4,13 @@ import "fmt"
 
 func main() {
 	fmt.Println(Sum(2, 2))
+	fmt.Println(Subtract(2, 2))
 }
 
 func Sum(a int, b int) int {
 	return a + b
+}
+
+func Subtract(a int, b int) int {
+	return a - b
 }
